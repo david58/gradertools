9d60a078dc28b5ab421b6ae449645dee35316d6c
Dávid <davidb@ksp.sk>
2017-01-12 19:00:55 +0100

    sample minimalistic c++ and python execute

--- gradertools/compilation/compile_cpp.py
+++ gradertools/compilation/compile_cpp.py
@@ -1,8 +1,20 @@
+import subprocess
+import shutil
+import os
 from .interface import CompilerInterface
 
 
 class CompilerCpp(CompilerInterface):
     def compile(self):
-        # TODO
+        sp = self.sourcepath
+        box = subprocess.run(['../isolate/isolate', '--init'], check=True, stdout=subprocess.PIPE).stdout
+        box = str(box.strip(), 'utf-8', errors='ignore')
+        shutil.copyfile(sp, os.path.join(box, 'box', 'source.cpp'))
+        out = subprocess.run(['../isolate/isolate', '--env=PATH=/usr/bin', '--processes', '--dir=/etc', '--run',
+                              '--',
+                              '/usr/bin/g++', '-o', 'compiledbinary', 'source.cpp'],
+                             check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        shutil.copy(os.path.join(box, 'box', 'compiledbinary'), 'compiledbinary')
         self._binarypath = 'compiledbinary'
-        print("cpp")
+        subprocess.run(['../isolate/isolate', '--cleanup'], check=True)
+        print('c++')
--- gradertools/execute/execute_binary.py
+++ gradertools/execute/execute_binary.py
@@ -1,6 +1,16 @@
+import subprocess
+import shutil
+import os
 from .interface import ExecuteInterface
 
 
 class ExecuteBinary(ExecuteInterface):
     def execute(self):
-        print("binary")
+        bp = self.binarypath
+        box = subprocess.run(['../isolate/isolate', '--init'], check=True, stdout=subprocess.PIPE).stdout
+        box = str(box.strip(), 'utf-8', errors='ignore')
+        shutil.copy(bp, os.path.join(box, 'box', 'binary'))
+        out = subprocess.run(['../isolate/isolate', '--run', 'binary'],
+                             check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(out.stdout)
+        subprocess.run(['../isolate/isolate', '--cleanup'], check=True)
