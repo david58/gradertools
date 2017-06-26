61a1b2ed0df7bea7e472a7e5300962a8c1c3710d
Dávid <davidb@ksp.sk>
2017-06-26 19:24:41 +0200

    pár riadkov kódu

--- gradertools/command/command.py
+++ gradertools/command/command.py
@@ -1,9 +1,24 @@
-class command:
-    files
-    command
-    parameters
-    envvariables
-    directories
-    allowmultiprocess
-    stdinfile
-    stdoutfile
+class Command:
+    def __init__(self, binary, args=[], input=None, output=None, wait=False):
+        self.commands = []
+        self.commands.append({
+            'execute':binary,
+            'args':args,
+            'input':input,
+            'output':output,
+            'wait':wait,
+        })
+        self._files = []
+        self._envvariables = []
+        self._directories = []
+        self._allowmultiprocess = False
+
+    def set_multiprocess(self, multiprocess):
+        self._allowmultiprocess = multiprocess
+
+#    def add_command(self,):
+
+#    def execute(self,isolator, timelimit, memlimit, ):
+
+#        return isolator.isolate(self._files, command, parameters, envvariables, directories, allowmultiprocess, stdinfile,
+#                                  stdoutfile)
